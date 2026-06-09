0249246f95d50a5418ab11bcf1b978909db7bb4c
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-09 11:55:51 -0600

    Update URL_APPS_SCRIPT in config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbz2Yix1cmgRoB8c4L6s0E63NERwry6txOSaCnPVYn7CeP4pruzXJH5_VRLQ0fJTe2k5dA/exec"
+  URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbz1vDtmzPQNPVIqWH6UESEGB_vcZdtMVwp-Fz1nGwbpDaqUkQxOyGpKHnEPpHgsVG3xTA/exec"
 };
